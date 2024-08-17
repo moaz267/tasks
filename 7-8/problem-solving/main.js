fe7e13353a7cf88b3ee52f82409e391bb4ff9e20
MAOZ <161624227+moaz267@users.noreply.github.com>
2024-08-17 22:11:30 +0300

    Add files via upload

---
+++ 7-8/problem-solving/main.js
@@ -0,0 +1,107 @@
+// let n =prompt("Enter number");
+// let text =prompt("Enter text");
+// let pass = prompt("Enter password");
+// let confirm_pass = prompt(" confirm password");
+// let day= prompt("Enter day");
+// let grade = prompt("Enter your grade");
+// let temp = prompt("What is the temperature?");
+
+// -------------------------------------------------------------------------------
+// 1-
+// if(n<=100){
+//     console.log("Number Is Less than Or Equal 100");
+// }
+// else{
+//     console.log("Number Is Greater than 100");
+// }
+// -------------------------------------------------------------------------------
+// 2-
+// if (text.length >100){
+//     console.log("string is long ");
+// }
+// else{
+//     console.log("string is short ");
+// }
+// -------------------------------------------------------------------------------
+// 3-
+// if(pass==confirm_pass){
+//     console.log("valid passwordd");
+// }
+// else{
+//     console.log("no valid password");
+// }
+// -------------------------------------------------------------------------------
+// 4-
+// if (day=="saturday" ){
+//     console.log("weekend");
+// }
+// else{
+//     console.log("work day");
+// }
+// -------------------------------------------------------------------------------
+// 5-
+// if(n>0){
+//     console.log("positive");
+// }
+// else if(n<0){
+//     console.log("negative");
+// }
+// else{
+//     console.log("zero");
+// }
+// -------------------------------------------------------------------------------
+// 6-  
+// if(grade<50){
+//     console.log("F");
+// }
+// else if(grade>60){
+//     console.log("D");
+// }
+// else if(grade>70){
+//     console.log("C");
+// }
+// else if(grade>80){
+//     console.log("B");
+// }
+// else{
+//     console.log("A");
+// }
+// -------------------------------------------------------------------------------
+// 7-
+// if(temp>30){
+//     console.log("hot");
+// }
+// else{
+//     console.log("cold");
+// }
+// -------------------------------------------------------------------------------
+// 8-
+// if(n%2==0){
+//     console.log("even");
+// }
+// else if (n%2==1){
+//     console.log("odd");
+// }
+// else{
+//     console.log("invalid");
+// }
+// -------------------------------------------------------------------------------
+// 9-
+// if (n%2==0)
+//     if(n>20){
+//         console.log("You can enter the university");
+//     }
+//     else{
+//         console.log("You can not enter the university");
+//     }
+// else if (n%2==1)
+//     if(n>20){
+//         console.log("You can enter the university");
+//     }
+//     else{
+//         console.log("You can not enter the university");
+//     }
+// else{
+//     console.log("invalid");
+// }
+
